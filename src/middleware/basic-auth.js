6032efaaf148c4a7fa525b4a5f0a79e5e2531e67
mgwedd <michael.wedd@leverege.com>
2019-08-03 18:59:11 -0400

    implemented JWT across protected endpoints

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,22 +1,17 @@
-const bcrypt = require( 'bcryptjs' )
 const AuthService = require( '../auth/auth-service' )
 
 function requireAuth( req, res, next ) {
   const authToken = req.get( 'Authorization' ) || ''
-
   let basicToken
   if ( !authToken.toLowerCase().startsWith( 'basic ' ) ) {
     return res.status( 401 ).json( { error : 'Missing basic token' } )
   } 
   basicToken = authToken.slice( 'basic '.length, authToken.length )
   
-
   const [ tokenUserName, tokenPassword ] = AuthService.parseBasicToken( basicToken )
-
   if ( !tokenUserName || !tokenPassword ) {
     return res.status( 401 ).json( { error : 'Unauthorized request' } )
   }
-
   AuthService.getUserWithUserName(
     req.app.get( 'db' ),
     tokenUserName
@@ -36,7 +31,6 @@ function requireAuth( req, res, next ) {
     } )
     .catch( next )
 }
-
 module.exports = {
   requireAuth,
 }
--- test/comments-endpoints.spec.js
+++ test/comments-endpoints.spec.js
@@ -2,7 +2,7 @@ const knex = require( 'knex' )
 const app = require( '../src/app' )
 const helpers = require( './test-helpers' )
 
-describe( 'Comments Endpoints', () => {
+describe.only( 'Comments Endpoints', () => {
   let db
 
   const {
